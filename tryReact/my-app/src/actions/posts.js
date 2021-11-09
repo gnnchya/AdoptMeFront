0a8fbe8eac2c0f5bed7183ccfb768d946e9a7056
gnnchya <75212443+gnnchya@users.noreply.github.com>
2021-11-09 15:16:52 +0700

    update config

--- tryReact/my-app/src/actions/posts.js
+++ tryReact/my-app/src/actions/posts.js
@@ -2,8 +2,8 @@ import axios from 'axios'
 import {data} from '../pages/readAllPostAdopt'
 
 export const createPost = (data) => axios.post("http://127.0.0.1:8080/LostPetPost", data)
-export const readAllPostAdopt = (type,limit,page) => axios.get(`http://127.0.0.1:8080/AdoptionPost/adopt?keyword=${type}limit=${limit}&page=${page}`)
-export const readAllPostLost = (limit,page) => axios.get(`http://127.0.0.1:8080/LostPetPost?limit=${limit}&page=${page}`)
+export const readAllPostAdopt = (type,limit,page) => axios.get(`http://127.0.0.1:8080/AdoptionPost?keyword=${type}&limit=${limit}&page=${page}`)
+export const readAllPostLost = (type,limit,page) => axios.get(`http://127.0.0.1:8080/LostPetPost?keyword=${type}&limit=${limit}&page=${page}`)
 // export const searchPostAdopt = (keyword,limit,page) => axios.get(`http://127.0.0.1:8080/posts/adopt?keyword=${keyword}limit=${limit}&page=${page}`)
 // export const searchPostLost = (keyword,limit,page) => axios.get(`http://127.0.0.1:8080/posts/lost?keyword=${keyword}limit=${limit}&page=${page}`)
 export const readPostAdopt = (id) => axios.get(`http://127.0.0.1:8080/AdoptionPost/${id}`)
