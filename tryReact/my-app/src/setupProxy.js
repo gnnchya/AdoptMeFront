b7472f16eeb40958acc18cdf139ba1c2ac51a036
pondnarawich <pondnarawich@gmail.com>
2021-12-14 22:04:04 +0700

    set up proxy

---
+++ tryReact/my-app/src/setupProxy.js
@@ -0,0 +1,11 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'http://0.0.0.0:3000',
+      changeOrigin: true,
+    })
+  );
+};
