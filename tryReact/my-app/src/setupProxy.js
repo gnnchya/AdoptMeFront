1b44634ccf38ecc98c77a3c7cf08b554a2133771
pondnarawich <pondnarawich@gmail.com>
2021-12-14 22:32:48 +0700

    Update setupProxy.js

--- tryReact/my-app/src/setupProxy.js
+++ tryReact/my-app/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     // '/AdoptMe',
     createProxyMiddleware({
-      target: 'http://0.0.0.0:3000',
+      target: 'http://0.0.0.0:80',
       changeOrigin: true,
     })
   );
